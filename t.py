073b2c84b47634534f26140ce2aa05752242e4a8
Musaibazad <86839201+Musaibazad@users.noreply.github.com>
2021-08-16 20:27:29 +0530

    Add files via upload

---
+++ t.py
@@ -0,0 +1,55 @@
+students=["Adnan","Basit","Rohan"]
+students.append("Khalid")
+print(students)
+
+
+students=["Adnan","Basit","Rohan"]
+students.clear()
+print(students)
+
+
+students=["Adnan","Basit","Rohan"]
+x = students.copy()
+print(x)
+
+
+
+students=["Adnan","Basit","Rohan"]
+x = students.count("Rohan")
+print(x)
+
+
+students=["Adnan","Basit","Rohan"]
+teachers = ['Gotham','Komal','Muneer']
+students.extend(teachers)
+print(students)
+
+
+students=["Adnan","Basit","Rohan"]
+x = students.index("Rohan")
+print(x)
+
+
+students=["Adnan","Basit","Rohan"]
+students.insert(1,"Khalid")
+print(students)
+
+students=["Adnan","Basit","Rohan"]
+students.pop(1)
+print(students)
+
+
+
+students=["Adnan","Basit","Rohan"]
+students.remove("Basit")
+print(students)
+
+
+students=["Adnan","Basit","Rohan"]
+students.reverse()
+print(students)
+
+teachers = ['Gotham','Komal','Muneer']
+teachers.sort()
+print(teachers)
+
